d3b4ca64bff8acf55b877616d20e50a4ca03bbb7
John Flournoy <uoflournoy@gmail.com>
2016-10-20 15:09:33 -0700

    added kable for md output

--- broom-example.r
+++ broom-example.r
@@ -1,3 +1,5 @@
+library(knitr)
+library(dplyr) 
 # mtcars is a built-in data set
 lmfit1 <- lm(mpg ~ wt, mtcars)
 lmfit1
@@ -7,9 +9,9 @@ summary(lmfit1)
 # install.packages("broom")
 library(broom)
 # tidy makes a dataframe of coefficients.
-tidy(lmfit1)
+tidy(lmfit1) %>% kable(digits=2)
 # glance makes a one-row dataframe with statistics for the overall model (r-squared, aic, bic, etc.)
-glance(lmfit1)
+glance(lmfit1) %>% kable(digits=2)
 
 # update is a base R function that lets you update regression models (add predictors).
 # You can see that I am adding a predictor, "cyl," and creating a new regression model with it, "lmfit2."
@@ -20,4 +22,4 @@ lmfit3 <- update(lmfit2,~.+ wt:cyl)
 # rbind is base R for "bind these dataframes together vertically." 'cbind' connects dataframes together horizontally.
 rbind(glance(lmfit1),
       glance(lmfit2),
-      glance(lmfit3))
+      glance(lmfit3)) %>% kable(digits=2)
